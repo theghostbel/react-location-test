49117876f391f00dce7158fea86867613bb259b5
neoroma <neoroma@gmail.com>
2015-09-26 23:55:57 +0300

    Add some not working google map

--- src/map.jsx
+++ src/map.jsx
@@ -1,22 +1,15 @@
 var React = require('react')
+var Map = require('react-maps')
 
 module.exports = React.createClass({
   render: function() {
 
     var lat = this.props.position.coords && this.props.position.coords.latitude,
-      long = this.props.position.coords && this.props.position.coords.longitude,
-      imgSrc = "https://maps.googleapis.com/maps/api/staticmap?center=" + lat + "," + long + "&zoom=13&size=300x300&sensor=true"
+      long = this.props.position.coords && this.props.position.coords.longitude
+      // imgSrc = "https://maps.googleapis.com/maps/api/staticmap?center=" + lat + "," + long + "&zoom=13&size=300x300&sensor=true"
 
     return (
-      <div className="map">
-        <div className="coords-row">
-          <span className="label label-info coord-unit">{lat}°</span>
-          <span className="label label-info coord-unit">{long}°</span>
-        </div>
-        <div className="map-picture">
-          <img src={imgSrc}/>
-        </div>
-      </div>
+      <Map latitude={lat} longitude={long} zoom={10}/>
     )
   }
 })
