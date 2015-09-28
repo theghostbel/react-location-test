0ff33087b53ee5c66bbd5cbb20f65a7bb0c304a6
neoroma <neoroma@gmail.com>
2015-09-28 22:54:17 +0300

    Add map loading

--- src/map.jsx
+++ src/map.jsx
@@ -1,6 +1,60 @@
 var React = require('react')
 
+var ReactScriptLoaderModule = require('react-script-loader');
+var ReactScriptLoaderMixin = ReactScriptLoaderModule.ReactScriptLoaderMixin;
+var ReactScriptLoader = ReactScriptLoaderModule.ReactScriptLoader;
+
+var scriptURL = 'http://maps.googleapis.com/maps/api/js?key=AIzaSyB-lE0R1V0ti3AjwQvdeK2EPu8p2P5Uu4A&sensor=true&callback=initializeMaps';
+
+// This function is called by the Google maps API after its initialization is
+// complete.
+// We need to define this function in the window scope to make it accessible
+// to the Google maps script.
+window.initializeMaps = function() {
+
+  // This triggers the onScriptLoaded method call on all mounted Map components.
+  ReactScriptLoader.triggerOnScriptLoaded(scriptURL);
+}
+
 module.exports = React.createClass({
+  mixins: [ReactScriptLoaderMixin],
+
+  // Ensure that onScriptLoaded is deferred until the
+  // ReactScriptLoader.triggerOnScriptLoaded() call above is made in
+  // initializeMaps().
+  deferOnScriptLoaded: function() {
+    return true;
+  },
+
+  getScriptURL: function() {
+    return scriptURL;
+  },
+
+  onScriptLoaded: function() {
+    // Render a map with the center point given by the component's lat and lng
+    // properties.
+
+    var lat = this.props.position.coords && this.props.position.coords.latitude,
+      long = this.props.position.coords && this.props.position.coords.longitude
+
+    var center = new google.maps
+      .LatLng(lat, long);
+    var mapOptions = {
+      zoom: 15,
+      center: center,
+      disableDefaultUI: true,
+      draggable: false,
+      zoomControl: false,
+      scrollwheel: false,
+      disableDoubleClickZoom: true
+    };
+    var map = new google.maps
+      .Map(this.getDOMNode(), mapOptions);
+  },
+  onScriptError: function() {
+    // Show the user an error message.
+  },
+
   render: function() {
 
     var lat = this.props.position.coords && this.props.position.coords.latitude,
@@ -8,15 +62,7 @@ module.exports = React.createClass({
       imgSrc = "https://maps.googleapis.com/maps/api/staticmap?center=" + lat + "," + long + "&zoom=15&size=400x400&sensor=true"
 
     return (
-      <div className="map">
-        <div className="coords-row">
-          <span className="label label-info coord-unit">{lat}°</span>
-          <span className="label label-info coord-unit">{long}°</span>
-        </div>
-        <div className="map-picture">
-          <img src={imgSrc}/>
-        </div>
-      </div>
+      <div className="map mapCanvas"></div>
     )
   }
 })
