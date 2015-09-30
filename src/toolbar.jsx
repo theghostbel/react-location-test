e53f2d78597009f0c65c947e0cef4ff6376079be
Roman Shestakov <neoroma@gmail.com>
2015-09-30 15:44:59 +0300

    Add another reading method

--- src/toolbar.jsx
+++ src/toolbar.jsx
@@ -15,6 +15,29 @@ module.exports = React.createClass({
   handleButtonClick: function() {
     this.updateCoords()
   },
+  handleSecondaryBtnClick: function() {
+    var self = this,
+      state = self.state
+
+    self.setState({
+      reading: true
+    })
+
+    if (geo.canBeUsed()) {
+      geo
+        .getCoords(function(pos) {
+          self.setState({
+            latitude: pos.coords.latitude,
+            longitude: pos.coords.longitude,
+            reading: false
+          })
+          self.props
+            .coordsCallback(null, pos)
+        }, function(err) {
+          alert(err)
+        })
+    }
+  },
   updateCoords: function() {
     var self = this,
       state = self.state
@@ -46,7 +69,8 @@ module.exports = React.createClass({
         <span className={state.reading
           ? ""
           : "invis"}>Reading</span>
-        <button className="btn btn-default btn-lg" onClick={self.handleButtonClick} type="button">Read my location</button>
+        <button className="btn btn-default" onClick={self.handleButtonClick} type="button">Read my location</button>
+        <button className="btn btn-default" onClick={self.handleSecondaryBtnClick} type="button">Long read</button>
         <span className="label label-info coord-unit">LAT:{state.latitude}</span>
         <span className="label label-info coord-unit">LONG:{state.longitude}</span>
       </footer>
